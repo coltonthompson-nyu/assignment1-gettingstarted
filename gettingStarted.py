fbcdf5d3185c44c9a8ef304ba3af2358f6f6b21c
coltonthompson-nyu <cjt9861@nyu.edu>
2024-09-08 21:30:21 -0400

    test

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,27 +1,46 @@
 ### welcome_assignment_answers
 ### Input - All nine questions given in the assignment.
 ### Output - The right answer for the specific question.
+import hashlib
+
 
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
-    return(answer)
+    match question:
+        case "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+            answer = "pcap"
+        case "Are encoding and encryption the same? - Yes/No":
+            answer = "No"
+        case "Is it possible to decrypt a message without a key? - Yes/No":
+            answer = "Yes"
+        case "Is it possible to decode a message without a key? - Yes/No":
+            answer = "Yes"
+        case "Is a hashed message supposed to be un-hashed? - Yes/No":
+            answer = "No"
+        case "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+            answer = hashlib.sha256(b"cjt9861@nyu.edu").hexdigest()
+        case "Is MD5 a secured hashing algorithm? - Yes/No":
+            answer = "No"
+        case "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+            answer = 5
+        case "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+            answer = 3
+        case default:
+            ### you should understand why this else case should be included
+            ### what happens if there is a typo in one of the questions?
+            ### maybe put something here to flag an issue and catch errors
+            return "undefined question - please try again"
+    return answer
 # Complete all the questions.
 
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
+    #print(welcome_assignment_answers(debug_question))
+    #print(welcome_assignment_answers("What is the SHA256 hashing value of your NYU email and use the answer in your code - "))
+    #print(welcome_assignment_answers("What is the SHA256 hashing value of your NYU email and use the answer in your code "))
 
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
