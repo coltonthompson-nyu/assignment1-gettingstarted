9fb516d4f794807d8ccc9cacea5b0d73d21c9664
coltonthompson-nyu <cjt9861@nyu.edu>
2024-09-09 18:33:03 -0400

    corrections

--- gettingStarted.py
+++ gettingStarted.py
@@ -12,14 +12,13 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-        #answer = hashlib.sha256(b"cjt9861@nyu.edu").hexdigest()
-        answer = "2949ebd2b49b9c6e7431405895578732703cf239b88a8dbcf57317e50296ea57"
+        answer = hashlib.sha256(b"cjt9861@nyu.edu").hexdigest()
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
